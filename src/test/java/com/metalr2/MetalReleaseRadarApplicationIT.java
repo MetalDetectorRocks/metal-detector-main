71645bf3c9ebf611b4cb6e91f93d58e225bf454b
Daniel Wagner <wagner.daniel87@gmail.com>
2019-10-03 17:42:45 +0200

    issue#48: add maven-failsafe-plugin to execute integration tests after unit tests

--- src/test/java/com/metalr2/MetalReleaseRadarApplicationIT.java
+++ src/test/java/com/metalr2/MetalReleaseRadarApplicationIT.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-integrationtest.properties")
-class MetalReleaseRadarApplicationTest {
+class MetalReleaseRadarApplicationIT {
 
   @Test
   void contextLoads() {
