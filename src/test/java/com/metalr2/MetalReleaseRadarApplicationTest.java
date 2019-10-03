a12ae8b3057640f047cb14977a675bd8da7fbb4f
Daniel Wagner <wagner.daniel87@gmail.com>
2019-10-03 16:49:53 +0200

    issue#48: use integration test property file for main application class

--- src/test/java/com/metalr2/MetalReleaseRadarApplicationTest.java
+++ src/test/java/com/metalr2/MetalReleaseRadarApplicationTest.java
@@ -2,8 +2,10 @@ package com.metalr2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
 class MetalReleaseRadarApplicationTest {
 
   @Test
