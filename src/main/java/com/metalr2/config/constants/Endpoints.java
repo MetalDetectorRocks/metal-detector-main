1ed58d326884332d060dbd4982683a481607d723
Nils Dammenhayn <hedge.hog.1833@gmail.com>
2019-11-25 13:48:41 +0100

    cleanup

--- src/main/java/com/metalr2/config/constants/Endpoints.java
+++ src/main/java/com/metalr2/config/constants/Endpoints.java
@@ -29,7 +29,6 @@ public class Endpoints {
   public static class Frontend {
     public static final String HOME                  = "/search-artists";
     public static final String SEARCH_ARTISTS        = "/search-artists";
-    public static final String ARTIST_DETAILS        = "/artist-details";
     public static final String SETTINGS              = "/settings";
     public static final String PROFILE               = "/profile";
     public static final String ARTISTS_RELEASES      = "/artists-releases";
