714cb14103d79b09f57f83c870c1a3de33176837
Nils Dammenhayn <nils.dammenhayn@europace.de>
2019-12-28 01:36:41 +0100

    top menu enhanced https://trello.com/c/LKluNlSS

--- src/test/java/com/metalr2/web/controller/mvc/HomeControllerIT.java
+++ src/test/java/com/metalr2/web/controller/mvc/HomeControllerIT.java
@@ -3,9 +3,7 @@ package com.metalr2.web.controller.mvc;
 import com.metalr2.config.constants.Endpoints;
 import com.metalr2.config.constants.ViewNames;
 import com.metalr2.testutil.WithIntegrationTestProfile;
-import com.metalr2.web.controller.mvc.SearchArtistsController;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcSecurityAutoConfiguration;
@@ -14,20 +12,23 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@WebMvcTest(value = SearchArtistsController.class, excludeAutoConfiguration = MockMvcSecurityAutoConfiguration.class)
-class SearchArtistsControllerIT implements WithIntegrationTestProfile {
+@WebMvcTest(value = HomeController.class, excludeAutoConfiguration = MockMvcSecurityAutoConfiguration.class)
+class HomeControllerIT implements WithIntegrationTestProfile {
 
   @Autowired
   private MockMvc mockMvc;
 
   @Test
-  @DisplayName("Requesting '" + Endpoints.Frontend.SEARCH_ARTISTS + "' should return the view to search artists")
+  @DisplayName("Requesting '" + Endpoints.Frontend.HOME + "' should return the view to search artists")
   void get_should_return_search_artists_view() throws Exception {
-    mockMvc.perform(get(Endpoints.Frontend.SEARCH_ARTISTS))
+    mockMvc.perform(get(Endpoints.Frontend.HOME))
               .andExpect(status().isOk())
-              .andExpect(view().name(ViewNames.Frontend.SEARCH_ARTISTS))
+              .andExpect(view().name(ViewNames.Frontend.HOME))
               .andExpect(model().size(0))
               .andExpect(content().contentType("text/html;charset=UTF-8"))
               .andExpect(content().string(containsString("Search")));
