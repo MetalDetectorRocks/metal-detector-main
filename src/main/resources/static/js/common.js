077f31bbf3295e07c6b7a8e3bae856ec81a76e9d
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-01-08 21:12:09 +0100

    Removed upper pagination Implemented back-to-top button https://trello.com/c/tWeUtotx

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -12,3 +12,34 @@ function registerLogoutListener() {
 function toggleLoader(id) {
     document.getElementById(id).classList.toggle("loader");
 }
+
+/**
+ * Set the onscroll function
+ */
+$(document).ready(function () {
+    window.onscroll = function() {onScrollFunction()};
+});
+
+/**
+ * Function called on scrolling
+ */
+function onScrollFunction() {
+    const backToTopButton = $("#backToTopButton");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopButton.fadeIn();
+    } else {
+        backToTopButton.fadeOut();
+    }
+}
+
+/**
+ * Animates scrolling back to the top
+ */
+function scrollToTop() {
+    $('#backToTopButton').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+}
