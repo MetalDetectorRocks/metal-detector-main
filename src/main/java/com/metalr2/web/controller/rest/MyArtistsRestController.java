17557d85123036b0748bd7d7f92838f95e0ec7ec
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-01-18 17:33:53 +0100

    MyArtists page implemented https://trello.com/c/TWR59rSP

--- src/main/java/com/metalr2/web/controller/rest/MyArtistsRestController.java
+++ src/main/java/com/metalr2/web/controller/rest/MyArtistsRestController.java
@@ -6,6 +6,7 @@ import com.metalr2.web.dto.ArtistDto;
 import com.metalr2.web.dto.response.MyArtistsResponse;
 import com.metalr2.web.dto.response.Pagination;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
@@ -29,7 +30,7 @@ public class MyArtistsRestController {
 
   @GetMapping(produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
   public ResponseEntity<MyArtistsResponse> getMyArtists(@PageableDefault Pageable pageable) {
-    List<ArtistDto> artists = artistsService.findFollowedArtistsForCurrentUser(pageable);
+    List<ArtistDto> artists = artistsService.findFollowedArtistsForCurrentUser(PageRequest.of(pageable.getPageNumber()-1, pageable.getPageSize()));
     long totalArtists = artistsService.countFollowedArtistsForCurrentUser();
     Pagination pagination = new Pagination(totalArtists, pageable.getPageNumber(), pageable.getPageSize());
     MyArtistsResponse response = new MyArtistsResponse(artists, pagination);
