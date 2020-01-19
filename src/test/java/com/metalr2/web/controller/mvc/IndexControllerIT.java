af136c2c21a308a890b8309c268e0dd88129c7f8
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-01-19 22:04:39 +0100

    IndexControllerIt tests

--- src/test/java/com/metalr2/web/controller/mvc/IndexControllerIT.java
+++ src/test/java/com/metalr2/web/controller/mvc/IndexControllerIT.java
@@ -2,28 +2,53 @@ package com.metalr2.web.controller.mvc;
 
 import com.metalr2.config.constants.Endpoints;
 import com.metalr2.config.constants.ViewNames;
-import com.metalr2.testutil.WithIntegrationTestProfile;
+import com.metalr2.testutil.WithSecurityConfig;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcSecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@WebMvcTest(value = IndexController.class, excludeAutoConfiguration = MockMvcSecurityAutoConfiguration.class)
-class IndexControllerIT implements WithIntegrationTestProfile {
+//@AutoConfigureMockMvc
+//@ContextConfiguration(classes = {IndexController.class})
+@WebMvcTest(IndexController.class)
+//@SpringBootTest
+class IndexControllerIT implements WithSecurityConfig {
 
   @Autowired
   private MockMvc mockMvc;
 
-  @Test
-  void given_index_uri_then_return_index_view() throws Exception {
-    mockMvc.perform(get(Endpoints.Guest.SLASH_INDEX))
-            .andExpect(status().isOk())
-            .andExpect(view().name(ViewNames.Guest.INDEX));
+//  @BeforeEach
+//  public void setUp() {
+//    mockMvc = MockMvcBuilders.webAppContextSetup(context)
+//        .apply(springSecurity())
+//        .build();
+//  }
+
+  @ParameterizedTest(name = "[{index}] => Endpoint <{0}>")
+  @ValueSource(strings = {Endpoints.Guest.SLASH_INDEX, Endpoints.Guest.INDEX})
+  @DisplayName("GET on index should return index view for anonymous user")
+  @WithAnonymousUser
+  void given_index_uri_then_return_index_view(String endpoint) throws Exception {
+    mockMvc.perform(get(endpoint))
+        .andExpect(status().isOk())
+        .andExpect(view().name(ViewNames.Guest.INDEX));
   }
 
+  @Test
+  @DisplayName("GET on index should return home view for logged in user")
+  @WithMockUser
+  void given_index_uri_then_return_home_view() throws Exception {
+    mockMvc.perform(get(Endpoints.Guest.SLASH_INDEX))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:" + ViewNames.Frontend.HOME));
+  }
 }
