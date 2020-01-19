6c7055df617077396a356d2225c9612f391dee9c
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-01-19 15:11:43 +0100

    Persists remember me token in database https://trello.com/c/XGhQPzn7

--- src/main/java/com/metalr2/config/jpa/DatabaseConfiguration.java
+++ src/main/java/com/metalr2/config/jpa/DatabaseConfiguration.java
@@ -1,18 +1,30 @@
 package com.metalr2.config.jpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.sql.DataSource;
+import java.util.Objects;
 import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
 public class DatabaseConfiguration {
+
+  private final Environment env;
+
+  @Autowired
+  public DatabaseConfiguration(Environment env) {
+    this.env = env;
+  }
 
   @Bean
   public AuditorAware<String> auditorAware() {
@@ -32,4 +44,13 @@ public class DatabaseConfiguration {
     }
   }
 
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("spring.datasource.driver-class-name")));
+    dataSource.setUrl(env.getProperty("spring.datasource.url"));
+    dataSource.setUsername(env.getProperty("spring.datasource.username"));
+    dataSource.setPassword(env.getProperty("spring.datasource.password"));
+    return dataSource;
+  }
 }
