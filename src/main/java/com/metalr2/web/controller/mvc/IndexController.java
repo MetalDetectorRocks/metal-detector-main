bb87567779a6923f44178279dc7dbb5b02287416
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-01-19 23:05:03 +0100

    Refinements

--- src/main/java/com/metalr2/web/controller/mvc/IndexController.java
+++ src/main/java/com/metalr2/web/controller/mvc/IndexController.java
@@ -5,17 +5,16 @@ import com.metalr2.config.constants.ViewNames;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
   @GetMapping({Endpoints.Guest.INDEX, Endpoints.Guest.EMPTY_INDEX, Endpoints.Guest.SLASH_INDEX})
-  public String showIndex(Authentication authentication) {
+  public ModelAndView showIndex(Authentication authentication) {
     if (authentication != null && authentication.isAuthenticated()) {
-//      return new ModelAndView("redirect:" + ViewNames.Frontend.HOME);
-      return "redirect:" + ViewNames.Frontend.HOME;
+      return new ModelAndView("redirect:" + Endpoints.Frontend.HOME);
     }
-//    return new ModelAndView(ViewNames.Guest.INDEX);
-    return ViewNames.Guest.INDEX;
+    return new ModelAndView(ViewNames.Guest.INDEX);
   }
 }
--- src/test/java/com/metalr2/web/controller/mvc/IndexControllerIT.java
+++ src/test/java/com/metalr2/web/controller/mvc/IndexControllerIT.java
@@ -2,53 +2,67 @@ package com.metalr2.web.controller.mvc;
 
 import com.metalr2.config.constants.Endpoints;
 import com.metalr2.config.constants.ViewNames;
+import com.metalr2.service.user.UserService;
 import com.metalr2.testutil.WithSecurityConfig;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import javax.sql.DataSource;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-//@AutoConfigureMockMvc
-//@ContextConfiguration(classes = {IndexController.class})
 @WebMvcTest(IndexController.class)
-//@SpringBootTest
 class IndexControllerIT implements WithSecurityConfig {
 
   @Autowired
   private MockMvc mockMvc;
 
-//  @BeforeEach
-//  public void setUp() {
-//    mockMvc = MockMvcBuilders.webAppContextSetup(context)
-//        .apply(springSecurity())
-//        .build();
-//  }
+  @MockBean
+  private UserService userService;
+
+  @MockBean
+  private BCryptPasswordEncoder passwordEncoder;
+
+  @MockBean
+  private DataSource dataSource;
 
   @ParameterizedTest(name = "[{index}] => Endpoint <{0}>")
-  @ValueSource(strings = {Endpoints.Guest.SLASH_INDEX, Endpoints.Guest.INDEX})
+  @ValueSource(strings = {Endpoints.Guest.INDEX, Endpoints.Guest.SLASH_INDEX})
   @DisplayName("GET on index should return index view for anonymous user")
   @WithAnonymousUser
   void given_index_uri_then_return_index_view(String endpoint) throws Exception {
-    mockMvc.perform(get(endpoint))
+    ResultActions resultActions = mockMvc.perform(get(endpoint));
+
+    resultActions
         .andExpect(status().isOk())
-        .andExpect(view().name(ViewNames.Guest.INDEX));
+        .andExpect(view().name(ViewNames.Guest.INDEX))
+        .andExpect(model().size(0))
+        .andExpect(content().contentType("text/html;charset=UTF-8"));
   }
 
-  @Test
+  @ParameterizedTest(name = "[{index}] => Endpoint <{0}>")
+  @ValueSource(strings = {Endpoints.Guest.INDEX, Endpoints.Guest.SLASH_INDEX})
   @DisplayName("GET on index should return home view for logged in user")
   @WithMockUser
-  void given_index_uri_then_return_home_view() throws Exception {
-    mockMvc.perform(get(Endpoints.Guest.SLASH_INDEX))
+  void given_index_uri_then_return_home_view(String endpoint) throws Exception {
+    mockMvc.perform(get(endpoint))
         .andExpect(status().is3xxRedirection())
-        .andExpect(view().name("redirect:" + ViewNames.Frontend.HOME));
+        .andExpect(redirectedUrl(Endpoints.Frontend.HOME))
+        .andExpect(view().name("redirect:" + Endpoints.Frontend.HOME))
+        .andExpect(model().size(0));
   }
 }
