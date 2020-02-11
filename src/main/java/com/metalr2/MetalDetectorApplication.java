0c64f04f7bc3656c8634c3288412504c7a033feb
Daniel Wagner <wagner.daniel87@gmail.com>
2020-02-11 19:18:55 +0100

    rename main application

--- src/main/java/com/metalr2/MetalDetectorApplication.java
+++ src/main/java/com/metalr2/MetalDetectorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MetalReleaseRadarApplication {
+public class MetalDetectorApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(MetalReleaseRadarApplication.class, args);
+    SpringApplication.run(MetalDetectorApplication.class, args);
   }
 
 }
--- src/test/java/com/metalr2/MetalDetectorApplicationIT.java
+++ src/test/java/com/metalr2/MetalDetectorApplicationIT.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MetalReleaseRadarApplicationIT implements WithIntegrationTestProfile {
+class MetalDetectorApplicationIT implements WithIntegrationTestProfile {
 
   @Test
   void contextLoads() {
