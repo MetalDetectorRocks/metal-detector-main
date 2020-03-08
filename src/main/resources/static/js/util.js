62025379bd743b30b6ce75c3d35360d3c09414b0
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-03-08 18:30:28 +0100

    Review: css removed

--- src/main/resources/static/js/util.js
+++ src/main/resources/static/js/util.js
@@ -47,7 +47,7 @@ function createFollowArtistButton(artistName,artistId,isFollowed) {
   const followArtistButtonElement = document.createElement('button');
   followArtistButtonElement.id = "followArtistButton"+artistId;
   followArtistButtonElement.type = "button";
-  followArtistButtonElement.className = "btn btn-primary btn-dark font-weight-bold follow-artist-button";
+  followArtistButtonElement.className = "btn btn-primary btn-dark font-weight-bold mt-2";
   followArtistButtonElement.textContent = isFollowed ? "Unfollow" : "Follow";
   followArtistButtonElement.onclick =createOnClickFunctionFollowArtist(artistName,
     artistId,isFollowed,followArtistButtonElement);
