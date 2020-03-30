c532211a00109568e2406b3ad99caf12bfa3166e
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-03-30 09:43:27 +0200

    ReleaseButlerRestClient extended by import method https://trello.com/c/VUMMePvm

--- butler/src/main/java/rocks/metaldetector/butler/client/ReleaseButlerRestClient.java
+++ butler/src/main/java/rocks/metaldetector/butler/client/ReleaseButlerRestClient.java
@@ -1,13 +1,13 @@
 package rocks.metaldetector.butler.client;
 
+import rocks.metaldetector.butler.api.ButlerImportResponse;
 import rocks.metaldetector.butler.api.ButlerReleasesRequest;
 import rocks.metaldetector.butler.api.ButlerReleasesResponse;
-import rocks.metaldetector.butler.facade.dto.ReleaseDto;
-
-import java.util.List;
 
 public interface ReleaseButlerRestClient {
 
   ButlerReleasesResponse queryReleases(ButlerReleasesRequest request);
+
+  ButlerImportResponse importReleases();
 
 }
