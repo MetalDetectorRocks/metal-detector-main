2a9b0a76bfc7b1c21d87fab54e3462a4a02f7684
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-04-14 22:33:09 +0200

    Integration test to unit test MyArtistsRestController https://trello.com/c/x6hnzTvO/121-integrationtests-to-unittests

--- webapp/src/main/java/rocks/metaldetector/web/controller/rest/MyArtistsRestController.java
+++ webapp/src/main/java/rocks/metaldetector/web/controller/rest/MyArtistsRestController.java
@@ -1,18 +1,18 @@
 package rocks.metaldetector.web.controller.rest;
 
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import rocks.metaldetector.config.constants.Endpoints;
-import rocks.metaldetector.service.artist.ArtistsService;
 import rocks.metaldetector.service.artist.ArtistDto;
-import rocks.metaldetector.web.api.response.MyArtistsResponse;
+import rocks.metaldetector.service.artist.ArtistsService;
 import rocks.metaldetector.support.Pagination;
+import rocks.metaldetector.web.api.response.MyArtistsResponse;
 
 import java.util.List;
 
@@ -24,10 +24,11 @@ public class MyArtistsRestController {
   private final ArtistsService artistsService;
 
   @GetMapping(produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-  public ResponseEntity<MyArtistsResponse> getMyArtists(@PageableDefault Pageable pageable) {
-    List<ArtistDto> artists = artistsService.findFollowedArtistsForCurrentUser(pageable);
+  public ResponseEntity<MyArtistsResponse> getMyArtists(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                        @RequestParam(value = "size", defaultValue = "10") int size) {
+    List<ArtistDto> artists = artistsService.findFollowedArtistsForCurrentUser(PageRequest.of(page, size));
     long totalArtists = artistsService.countFollowedArtistsForCurrentUser();
-    Pagination pagination = new Pagination(totalArtists, pageable.getPageNumber(), pageable.getPageSize());
+    Pagination pagination = new Pagination(totalArtists, page, size);
     MyArtistsResponse response = new MyArtistsResponse(artists, pagination);
     return ResponseEntity.ok(response);
   }
