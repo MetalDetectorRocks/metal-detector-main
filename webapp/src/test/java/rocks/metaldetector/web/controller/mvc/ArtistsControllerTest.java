ade954da197419df5a76019a6bb6b6807ad30e08
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-04-15 17:55:24 +0200

    ArtistsControllerIT to ArtistsControllerTest https://trello.com/c/x6hnzTvO

---
+++ webapp/src/test/java/rocks/metaldetector/web/controller/mvc/ArtistsControllerTest.java
@@ -0,0 +1,55 @@
+package rocks.metaldetector.web.controller.mvc;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import rocks.metaldetector.config.constants.Endpoints;
+import rocks.metaldetector.config.constants.ViewNames;
+import rocks.metaldetector.web.RestAssuredMockMvcUtils;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+class ArtistsControllerTest {
+
+  @InjectMocks
+  private ArtistsController underTest;
+
+  private RestAssuredMockMvcUtils restAssuredUtils;
+
+  @BeforeEach
+  void setup() {
+    restAssuredUtils = new RestAssuredMockMvcUtils(Endpoints.Frontend.ARTISTS);
+    RestAssuredMockMvc.standaloneSetup(underTest,
+                                       springSecurity((request, response, chain) -> chain.doFilter(request, response)));
+  }
+
+  @Test
+  @DisplayName("Requesting '" + Endpoints.Frontend.ARTISTS + "' should be ok")
+  void get_search_should_return_200() {
+    // when
+    var validatableResponse = restAssuredUtils.doGet();
+
+    // then
+    validatableResponse.statusCode(HttpStatus.OK.value());
+  }
+
+  @Test
+  @DisplayName("Requesting '" + Endpoints.Frontend.ARTISTS + "' should return the view to search artists")
+  void get_search_should_return_search_artists_view() {
+    // when
+    var validatableResponse = restAssuredUtils.doGet();
+
+    // then
+    validatableResponse.assertThat(view().name(ViewNames.Frontend.SEARCH))
+        .assertThat(model().size(0))
+        .assertThat(model().hasNoErrors());
+  }
+}
