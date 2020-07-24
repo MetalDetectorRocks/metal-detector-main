7bbd90411466d8280f53856e84cb2a68bc4a14e1
Daniel Wagner <wagner.daniel87@gmail.com>
2020-07-24 15:30:09 +0200

    uncomment failing test

--- webapp/src/test/java/rocks/metaldetector/MetalDetectorApplicationDefaultIT.java
+++ webapp/src/test/java/rocks/metaldetector/MetalDetectorApplicationDefaultIT.java
@@ -1,5 +1,6 @@
 package rocks.metaldetector;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
@@ -12,9 +13,9 @@ import rocks.metaldetector.testutil.WithIntegrationTestConfig;
 })
 class MetalDetectorApplicationDefaultIT implements WithIntegrationTestConfig {
 
-//  @Test
-//  void contextLoads() {
-//    // Simple test to check if the Spring Application context can be loaded successfully with 'default' profile
-//  }
+  @Test
+  void contextLoads() {
+    // Simple test to check if the Spring Application context can be loaded successfully with 'default' profile
+  }
 
 }
