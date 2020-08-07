73f954e21705acf7eea5b22f69c52c9d896e0114
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-08-07 23:45:43 +0200

    TypeScript for Homepage with recently followed artists https://trello.com/c/hgvQjrHB

---
+++ webapp/src/main/resources/static/ts/src/clients/homepage-rest-client.ts
@@ -0,0 +1,22 @@
+import axios, {AxiosError, AxiosResponse} from "axios";
+import {axiosConfig} from "../config/axios.config";
+import {HomepageResponse} from "../model/homepage-response.model";
+
+export class HomepageRestClient {
+
+    private readonly HOMEPAGE_URL = "/rest/v1/home"
+
+    constructor() {
+    }
+
+    public async fetchHomepage(): Promise<HomepageResponse> {
+        return axios.get(
+          this.HOMEPAGE_URL, axiosConfig
+        ).then((response: AxiosResponse<HomepageResponse>) => {
+            return response.data;
+        }).catch((error: AxiosError) => {
+            console.error(error);
+            throw error;
+        });
+    }
+}
