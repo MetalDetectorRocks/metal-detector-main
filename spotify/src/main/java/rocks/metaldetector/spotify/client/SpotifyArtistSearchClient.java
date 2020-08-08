10039e4759ad196a399bb5ef29fce03b63c0af3f
Daniel Wagner <wagner.daniel87@gmail.com>
2020-08-08 15:53:48 +0200

    add searchById in spotify rest client

--- spotify/src/main/java/rocks/metaldetector/spotify/client/SpotifyArtistSearchClient.java
+++ spotify/src/main/java/rocks/metaldetector/spotify/client/SpotifyArtistSearchClient.java
@@ -1,9 +1,12 @@
 package rocks.metaldetector.spotify.client;
 
+import rocks.metaldetector.spotify.api.search.SpotifyArtist;
 import rocks.metaldetector.spotify.api.search.SpotifyArtistSearchResultContainer;
 
 public interface SpotifyArtistSearchClient {
 
   SpotifyArtistSearchResultContainer searchByName(String authenticationToken, String artistQueryString, int pageNumber, int pageSize);
+
+  SpotifyArtist searchById(String authenticationToken, String externalId);
 
 }
