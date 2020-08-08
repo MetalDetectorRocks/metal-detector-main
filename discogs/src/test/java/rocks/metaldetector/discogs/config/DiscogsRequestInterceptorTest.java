c1beecba4a8737ae3188eb39f2a5880375300a22
Daniel Wagner <wagner.daniel87@gmail.com>
2020-08-08 15:18:45 +0200

    add test for DiscogsRequestInterceptor

---
+++ discogs/src/test/java/rocks/metaldetector/discogs/config/DiscogsRequestInterceptorTest.java
@@ -0,0 +1,97 @@
+package rocks.metaldetector.discogs.config;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequestExecution;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static rocks.metaldetector.discogs.config.DiscogsRequestInterceptor.TOKEN_PREFIX;
+
+@ExtendWith(MockitoExtension.class)
+class DiscogsRequestInterceptorTest implements WithAssertions {
+
+  @Mock
+  private DiscogsConfig discogsConfig;
+
+  @Mock
+  private HttpRequest httpRequestMock;
+
+  @Mock
+  private HttpHeaders httpHeadersMock;
+
+  @Mock
+  private ClientHttpRequestExecution clientHttpRequestExecutionMock;
+
+  @InjectMocks
+  private DiscogsRequestInterceptor underTest;
+
+  @Test
+  @DisplayName("should set accept header always to 'application/json'")
+  void accept_header_is_set() throws IOException {
+    // given
+    doReturn(httpHeadersMock).when(httpRequestMock).getHeaders();
+
+    // when
+    underTest.intercept(httpRequestMock, new byte[]{}, clientHttpRequestExecutionMock);
+
+    // then
+    verify(httpHeadersMock, times(1)).setAccept(List.of(MediaType.APPLICATION_JSON));
+  }
+
+  @Test
+  @DisplayName("should set user agent with value from ButlerConfig")
+  void user_agent_header_is_set() throws IOException {
+    // given
+    var userAgent = "User Agent";
+    doReturn(httpHeadersMock).when(httpRequestMock).getHeaders();
+    doReturn(userAgent).when(discogsConfig).getUserAgent();
+
+    // when
+    underTest.intercept(httpRequestMock, new byte[]{}, clientHttpRequestExecutionMock);
+
+    // then
+    verify(httpHeadersMock, times(1)).set(HttpHeaders.USER_AGENT, userAgent);
+  }
+
+  @Test
+  @DisplayName("should set authorization header with token from ButlerConfig")
+  void authorization_header_is_set() throws IOException {
+    // given
+    var token = "discogs token";
+    doReturn(httpHeadersMock).when(httpRequestMock).getHeaders();
+    doReturn(token).when(discogsConfig).getAccessToken();
+
+    // when
+    underTest.intercept(httpRequestMock, new byte[]{}, clientHttpRequestExecutionMock);
+
+    // then
+    verify(httpHeadersMock, times(1)).set(HttpHeaders.AUTHORIZATION, TOKEN_PREFIX + token);
+  }
+
+  @Test
+  @DisplayName("should call 'execute' on ClientHttpRequestExecution")
+  void execute_is_called() throws IOException {
+    // given
+    var bodyAsByteArray = new byte[]{};
+    doReturn(httpHeadersMock).when(httpRequestMock).getHeaders();
+
+    // when
+    underTest.intercept(httpRequestMock, bodyAsByteArray, clientHttpRequestExecutionMock);
+
+    // then
+    verify(clientHttpRequestExecutionMock, times(1)).execute(httpRequestMock, bodyAsByteArray);
+  }
+}
