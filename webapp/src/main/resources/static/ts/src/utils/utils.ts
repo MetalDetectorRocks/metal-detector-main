9130d495223c27f26cc3347d4854f54844ba6715
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-08-24 10:01:54 +0200

    Util method is public https://trello.com/c/D9Na8oxO

--- webapp/src/main/resources/static/ts/src/utils/utils.ts
+++ webapp/src/main/resources/static/ts/src/utils/utils.ts
@@ -1,6 +1,6 @@
 export class Utils {
 
-    static calculateTimeRange(date: Date) {
+    public static calculateTimeRange(date: Date) {
         const now = Date.now();
         const then = new Date(date).valueOf();
         return Math.round((now - then) / 1000 / 60 / 60 / 24);
