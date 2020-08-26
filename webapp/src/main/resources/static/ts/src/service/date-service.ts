af8ff8e974afe0ae5e5da4d8485ac09539afeae7
hedgehog1833 <32484683+hedgehog1833@users.noreply.github.com>
2020-08-26 13:34:46 +0200

    Add return type
    Co-authored-by: Daniel <wagner.daniel87@gmail.com>

--- webapp/src/main/resources/static/ts/src/service/date-service.ts
+++ webapp/src/main/resources/static/ts/src/service/date-service.ts
@@ -9,7 +9,7 @@ export class DateService {
         return Math.round((now - then) / 1000 / 60 / 60 / 24);
     }
 
-    public calculateFutureTimeRange(date: Date) {
+    public calculateFutureTimeRange(date: Date): number {
         const now = Date.now();
         const then = new Date(date).valueOf();
         return Math.round((then - now) / 1000 / 60 / 60 / 24);
