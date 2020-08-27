3968d619b078b4444db2abd91566e8ea3b7e0517
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-08-27 15:54:39 +0200

    Review: SummaryServiceImpl fetches followed artists https://trello.com/c/hgvQjrHB

--- webapp/src/test/java/rocks/metaldetector/service/summary/ReleaseCollectorTest.java
+++ webapp/src/test/java/rocks/metaldetector/service/summary/ReleaseCollectorTest.java
@@ -8,7 +8,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import rocks.metaldetector.butler.facade.ReleaseService;
-import rocks.metaldetector.service.artist.FollowArtistService;
 import rocks.metaldetector.support.PageRequest;
 import rocks.metaldetector.support.TimeRange;
 import rocks.metaldetector.testutil.DtoFactory.ArtistDtoFactory;
@@ -19,7 +18,6 @@ import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
@@ -32,33 +30,14 @@ class ReleaseCollectorTest implements WithAssertions {
   @Mock
   private ReleaseService releaseService;
 
-  @Mock
-  private FollowArtistService followArtistService;
-
   @InjectMocks
   private ReleaseCollector underTest;
 
   @Test
-  @DisplayName("collecting upcoming releases calls followArtistService")
-  void test_upcoming_releases_calls_follow_artist_service() {
-    // given
-    doReturn(List.of(ArtistDtoFactory.withName("A"))).when(followArtistService).getFollowedArtistsOfCurrentUser();
-
-    // when
-    underTest.collectUpcomingReleases();
-
-    // then
-    verify(followArtistService, times(1)).getFollowedArtistsOfCurrentUser();
-  }
-
-  @Test
-  @DisplayName("collecting upcoming releases does not call releaseService when no artists are followed")
+  @DisplayName("collecting upcoming releases does not call releaseService when no artists are given")
   void test_upcoming_releases_does_not_call_release_service() {
-    // given
-    doReturn(Collections.emptyList()).when(followArtistService).getFollowedArtistsOfCurrentUser();
-
     // when
-    underTest.collectUpcomingReleases();
+    underTest.collectUpcomingReleases(Collections.emptyList());
 
     // then
     verifyNoInteractions(releaseService);
@@ -69,10 +48,10 @@ class ReleaseCollectorTest implements WithAssertions {
   void test_upcoming_releases_calls_release_service_with_artist_names() {
     // given
     var expectedArtistNames = List.of("A");
-    doReturn(List.of(ArtistDtoFactory.withName("A"))).when(followArtistService).getFollowedArtistsOfCurrentUser();
+    var artists = List.of(ArtistDtoFactory.withName("A"));
 
+    underTest.collectUpcomingReleases(artists);
     // when
-    underTest.collectUpcomingReleases();
 
     // then
     verify(releaseService, times(1)).findReleases(eq(expectedArtistNames), any(), any());
@@ -84,10 +63,10 @@ class ReleaseCollectorTest implements WithAssertions {
     // given
     var now = LocalDate.now();
     var expectedTimeRange = new TimeRange(now, now.plusMonths(TIME_RANGE_MONTHS));
-    doReturn(List.of(ArtistDtoFactory.withName("A"))).when(followArtistService).getFollowedArtistsOfCurrentUser();
+    var artists = List.of(ArtistDtoFactory.withName("A"));
 
     // when
-    underTest.collectUpcomingReleases();
+    underTest.collectUpcomingReleases(artists);
 
     // then
     verify(releaseService, times(1)).findReleases(any(), eq(expectedTimeRange), any());
@@ -98,36 +77,20 @@ class ReleaseCollectorTest implements WithAssertions {
   void test_upcoming_releases_calls_release_service_with_page_request() {
     // given
     var expectedPageRequest = new PageRequest(1, RESULT_LIMIT);
-    doReturn(List.of(ArtistDtoFactory.withName("A"))).when(followArtistService).getFollowedArtistsOfCurrentUser();
+    var artists = List.of(ArtistDtoFactory.withName("A"));
 
     // when
-    underTest.collectUpcomingReleases();
+    underTest.collectUpcomingReleases(artists);
 
     // then
     verify(releaseService, times(1)).findReleases(any(), any(), eq(expectedPageRequest));
   }
 
   @Test
-  @DisplayName("collecting recent releases calls followArtistService")
-  void test_recent_releases_calls_follow_artist_service() {
-    // given
-    doReturn(List.of(ArtistDtoFactory.withName("A"))).when(followArtistService).getFollowedArtistsOfCurrentUser();
-
-    // when
-    underTest.collectRecentReleases();
-
-    // then
-    verify(followArtistService, times(1)).getFollowedArtistsOfCurrentUser();
-  }
-
-  @Test
-  @DisplayName("collecting recent releases does not call releaseService when no artists are followed")
+  @DisplayName("collecting recent releases does not call releaseService when no artists are given")
   void test_recent_releases_does_not_call_release_service() {
-    // given
-    doReturn(Collections.emptyList()).when(followArtistService).getFollowedArtistsOfCurrentUser();
-
     // when
-    underTest.collectRecentReleases();
+    underTest.collectRecentReleases(Collections.emptyList());
 
     // then
     verifyNoInteractions(releaseService);
@@ -138,10 +101,10 @@ class ReleaseCollectorTest implements WithAssertions {
   void test_recent_releases_calls_release_service_with_artist_names() {
     // given
     var expectedArtistNames = List.of("A");
-    doReturn(List.of(ArtistDtoFactory.withName("A"))).when(followArtistService).getFollowedArtistsOfCurrentUser();
+    var artists = List.of(ArtistDtoFactory.withName("A"));
 
     // when
-    underTest.collectRecentReleases();
+    underTest.collectRecentReleases(artists);
 
     // then
     verify(releaseService, times(1)).findReleases(eq(expectedArtistNames), any(), any());
@@ -153,10 +116,10 @@ class ReleaseCollectorTest implements WithAssertions {
     // given
     var now = LocalDate.now();
     var expectedTimeRange = new TimeRange(now.minusMonths(TIME_RANGE_MONTHS), now);
-    doReturn(List.of(ArtistDtoFactory.withName("A"))).when(followArtistService).getFollowedArtistsOfCurrentUser();
+    var artists = List.of(ArtistDtoFactory.withName("A"));
 
     // when
-    underTest.collectRecentReleases();
+    underTest.collectRecentReleases(artists);
 
     // then
     verify(releaseService, times(1)).findReleases(any(), eq(expectedTimeRange), any());
@@ -167,63 +130,10 @@ class ReleaseCollectorTest implements WithAssertions {
   void test_recent_releases_calls_release_service_with_page_request() {
     // given
     var expectedPageRequest = new PageRequest(1, RESULT_LIMIT);
-    doReturn(List.of(ArtistDtoFactory.withName("A"))).when(followArtistService).getFollowedArtistsOfCurrentUser();
+    var artists = List.of(ArtistDtoFactory.withName("A"));
 
     // when
-    underTest.collectRecentReleases();
-
-    // then
-    verify(releaseService, times(1)).findReleases(any(), any(), eq(expectedPageRequest));
-  }
-
-  @Test
-  @DisplayName("collecting upcoming releases for given artists does not call releaseService when artists are empty")
-  void test_upcoming_releases_for_given_artists_does_not_call_release_service() {
-    // when
-    underTest.collectUpcomingReleases(Collections.emptyList());
-
-    // then
-    verifyNoInteractions(releaseService);
-  }
-
-  @Test
-  @DisplayName("collecting upcoming releases for given artists calls releaseService with artists' names")
-  void test_upcoming_releases_for_given_artists_calls_release_service_with_artist_names() {
-    // given
-    var expectedArtistNames = List.of("A");
-    var artists = List.of(ArtistDtoFactory.createDefault());
-
-    // when
-    underTest.collectUpcomingReleases(artists);
-
-    // then
-    verify(releaseService, times(1)).findReleases(eq(expectedArtistNames), any(), any());
-  }
-
-  @Test
-  @DisplayName("collecting upcoming releases for given artists calls releaseService with correct time range")
-  void test_upcoming_releases_for_given_artists_calls_release_service_with_time_range() {
-    // given
-    var now = LocalDate.now();
-    var expectedTimeRange = new TimeRange(now, now.plusMonths(TIME_RANGE_MONTHS));
-    var artists = List.of(ArtistDtoFactory.createDefault());
-
-    // when
-    underTest.collectUpcomingReleases(artists);
-
-    // then
-    verify(releaseService, times(1)).findReleases(any(), eq(expectedTimeRange), any());
-  }
-
-  @Test
-  @DisplayName("collecting upcoming releases for given artists calls releaseService with correct page request")
-  void test_upcoming_releases_for_given_artists_calls_release_service_with_page_request() {
-    // given
-    var expectedPageRequest = new PageRequest(1, RESULT_LIMIT);
-    var artists = List.of(ArtistDtoFactory.createDefault());
-
-    // when
-    underTest.collectUpcomingReleases(artists);
+    underTest.collectRecentReleases(artists);
 
     // then
     verify(releaseService, times(1)).findReleases(any(), any(), eq(expectedPageRequest));
