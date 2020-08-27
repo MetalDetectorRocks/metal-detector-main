c723cd328a9dbe45f9f892b04037181a4000ae5b
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-08-27 13:04:01 +0200

    Favorite community artists are fetched and displayed on homepage Placeholder card if not enough results are present for releases Only one release row if not enough results are present for two rows https://trello.com/c/D9Na8oxO

--- webapp/src/main/java/rocks/metaldetector/service/summary/SummaryServiceMock.java
+++ webapp/src/main/java/rocks/metaldetector/service/summary/SummaryServiceMock.java
@@ -27,19 +27,18 @@ public class SummaryServiceMock implements SummaryService {
   }
 
   private List<ReleaseDto> createUpcomingReleases(LocalDate now) {
-    ReleaseDto fromTheAbyss = ReleaseDto.builder().albumTitle("From The Abyss").artist("Drowning The Light").releaseDate(now.plusDays(6)).coverUrl("/images/dummy/drowning-the-light_from-the-abyss.jpg").build();
     ReleaseDto waldvolk = ReleaseDto.builder().albumTitle("Waldvolk").artist("XIV Dark Centuries").releaseDate(now.plusDays(11)).coverUrl("/images/dummy/xiv-dark-centuries_waldvolk.jpg").build();
     ReleaseDto transilvanianHunger = ReleaseDto.builder().albumTitle("Transilvanian Hunger").artist("Darkthrone").releaseDate(now.plusDays(21)).coverUrl("/images/dummy/darkthrone_transilvanian-hunger.jpg").build();
-    ReleaseDto traktat = ReleaseDto.builder().albumTitle("Traktat").artist("Karg").releaseDate(now.plusDays(25)).coverUrl("/images/dummy/karg_traktat.jpg").build();
-    return List.of(fromTheAbyss, waldvolk, transilvanianHunger, traktat);
+//    ReleaseDto traktat = ReleaseDto.builder().albumTitle("Traktat").artist("Karg").releaseDate(now.plusDays(25)).coverUrl("/images/dummy/karg_traktat.jpg").build();
+    return List.of(waldvolk, transilvanianHunger);
   }
 
   private List<ReleaseDto> createRecentReleases(LocalDate now) {
     ReleaseDto freierWilleFreierGeist = ReleaseDto.builder().albumTitle("Freier Wille, Freier Geist").artist("Thormesis").releaseDate(now.minusDays(2)).coverUrl("/images/dummy/thormesis_freier-wille-freier-geist.jpg").build();
     ReleaseDto sonsOfNorthernDarkness = ReleaseDto.builder().albumTitle("Sons of Northern Darkness").artist("Immortal").releaseDate(now.minusDays(13)).coverUrl("/images/dummy/immortal_sons-of_northern_darkness.jpg").build();
-    ReleaseDto ritualeSatanum = ReleaseDto.builder().albumTitle("Rituale Satanum").artist("Behexen").releaseDate(now.minusDays(25)).coverUrl("/images/dummy/behexen_rituale-satanum.jpg").build();
-    ReleaseDto ageOfExcuse = ReleaseDto.builder().albumTitle("Age of Excuse").artist("Mgla").releaseDate(now.minusDays(29)).coverUrl("/images/dummy/mgla_age-of-excuse.jpg").build();
-    return List.of(freierWilleFreierGeist, sonsOfNorthernDarkness, ritualeSatanum, ageOfExcuse);
+//    ReleaseDto ritualeSatanum = ReleaseDto.builder().albumTitle("Rituale Satanum").artist("Behexen").releaseDate(now.minusDays(25)).coverUrl("/images/dummy/behexen_rituale-satanum.jpg").build();
+//    ReleaseDto ageOfExcuse = ReleaseDto.builder().albumTitle("Age of Excuse").artist("Mgla").releaseDate(now.minusDays(29)).coverUrl("/images/dummy/mgla_age-of-excuse.jpg").build();
+    return List.of(freierWilleFreierGeist, sonsOfNorthernDarkness);
   }
 
   private List<ArtistDto> createRecentlyFollowedArtists(LocalDate now) {
