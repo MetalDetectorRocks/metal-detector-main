124dc8df508cb3791c30cea604a31a9da31bf4dd
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-09-02 13:21:31 +0200

    dateTo cannot be set without dateFrom in ReleasesRequests https://trello.com/c/yg3ecF7g

--- webapp/src/main/java/rocks/metaldetector/web/api/request/WithTimeRangeValidation.java
+++ webapp/src/main/java/rocks/metaldetector/web/api/request/WithTimeRangeValidation.java
@@ -9,11 +9,17 @@ public interface WithTimeRangeValidation {
 
   @AssertTrue(message = "If dates are set, dateFrom has to be equal to or before dateTo!")
   @ArtifactForFramework
-  default boolean isValid() {
+  default boolean isDateFromValid() {
     if (getDateFrom() != null && getDateTo() != null) {
       return getDateFrom().equals(getDateTo()) || getDateFrom().isBefore(getDateTo());
     }
     return true;
+  }
+
+  @AssertTrue(message = "dateTo must not be set without dateFrom!")
+  @ArtifactForFramework
+  default boolean isDateToValid() {
+    return getDateFrom() != null || getDateTo() == null;
   }
 
   LocalDate getDateFrom();
