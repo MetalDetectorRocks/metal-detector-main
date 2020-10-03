803b464ad207e22998f6aa32016937971e22dcd6
Daniel Wagner <wagner.daniel87@gmail.com>
2020-10-03 20:15:00 +0200

    show correct followedSince date in frontend

--- webapp/src/main/resources/static/ts/src/bundles/my-artists.ts
+++ webapp/src/main/resources/static/ts/src/bundles/my-artists.ts
@@ -1,16 +1,18 @@
-import { ArtistsRestClient } from "../clients/artists-rest-client";
-import { MyArtistsRenderService } from "../service/my-artists-render-service";
-import { FollowArtistService } from "../service/follow-artist-service";
-import { ToastService } from "../service/toast-service";
-import { AlertService } from "../service/alert-service";
-import { LoadingIndicatorService } from "../service/loading-indicator-service";
+import {ArtistsRestClient} from "../clients/artists-rest-client";
+import {MyArtistsRenderService} from "../service/my-artists-render-service";
+import {FollowArtistService} from "../service/follow-artist-service";
+import {ToastService} from "../service/toast-service";
+import {AlertService} from "../service/alert-service";
+import {LoadingIndicatorService} from "../service/loading-indicator-service";
+import {DateFormatService} from "../service/date-format-service";
 
 const toastService = new ToastService();
 const alertService = new AlertService();
 const loadingIndicatorService = new LoadingIndicatorService();
 const artistsRestClient = new ArtistsRestClient(toastService);
 const followArtistService = new FollowArtistService(artistsRestClient, toastService);
-const myArtistsRenderService = new MyArtistsRenderService(followArtistService, alertService, loadingIndicatorService);
+const dateFormatService = new DateFormatService();
+const myArtistsRenderService = new MyArtistsRenderService(followArtistService, dateFormatService, alertService, loadingIndicatorService);
 
 const response = artistsRestClient.fetchMyArtists();
 myArtistsRenderService.render(response);
--- webapp/src/test/java/rocks/metaldetector/service/artist/ArtistTransformerTest.java
+++ webapp/src/test/java/rocks/metaldetector/service/artist/ArtistTransformerTest.java
@@ -4,7 +4,11 @@ import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import rocks.metaldetector.persistence.domain.artist.ArtistEntity;
+import rocks.metaldetector.persistence.domain.artist.FollowActionEntity;
 import rocks.metaldetector.persistence.domain.artist.TopArtist;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -43,5 +47,25 @@ class ArtistTransformerTest implements WithAssertions {
     // then
     assertThat(result.getArtistName()).isEqualTo(topArtist.getArtistName());
     assertThat(result.getThumb()).isEqualTo(topArtist.getThumb());
+  }
+
+  @Test
+  @DisplayName("Should transform FollowActionEntity to ArtistDto")
+  void should_transform_follow_action_entity_to_artist_dto() {
+    // given
+    FollowActionEntity followAction = mock(FollowActionEntity.class);
+    ArtistEntity artistEntity = ArtistEntityFactory.withExternalId("1");
+    doReturn(artistEntity).when(followAction).getArtist();
+    doReturn(LocalDateTime.of(2020, 1, 1, 0, 0, 0)).when(followAction).getCreatedDateTime();
+
+    // when
+    ArtistDto result = underTest.transform(followAction);
+
+    // then
+    assertThat(result.getExternalId()).isEqualTo(artistEntity.getExternalId());
+    assertThat(result.getArtistName()).isEqualTo(artistEntity.getArtistName());
+    assertThat(result.getThumb()).isEqualTo(artistEntity.getThumb());
+    assertThat(result.getSource()).isEqualTo(artistEntity.getSource().getDisplayName());
+    assertThat(result.getFollowedSince()).isEqualTo(LocalDate.of(2020, 1, 1));
   }
 }
