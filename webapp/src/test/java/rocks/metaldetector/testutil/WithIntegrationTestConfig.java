79c12c51628639fee3e1c64cdd351de6629fb01e
Nils Dammenhayn <nils.dammenhayn@europace.de>
2020-10-10 19:56:16 +0200

    Tests repaired https://trello.com/c/jTFeZAZc

--- webapp/src/test/java/rocks/metaldetector/testutil/WithIntegrationTestConfig.java
+++ webapp/src/test/java/rocks/metaldetector/testutil/WithIntegrationTestConfig.java
@@ -4,6 +4,6 @@ import org.junit.jupiter.api.Tag;
 import org.springframework.test.context.TestPropertySource;
 
 @Tag("integration-test")
-@TestPropertySource(locations = "classpath:integrationtest.properties")
+@TestPropertySource(locations = "classpath:integrationtest.yml")
 public interface WithIntegrationTestConfig {
 }
