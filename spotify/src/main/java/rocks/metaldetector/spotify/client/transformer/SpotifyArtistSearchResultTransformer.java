298ee886142c500bc6f7b8235aa7042dc409dd1c
Daniel Wagner <wagner.daniel87@gmail.com>
2020-10-22 19:24:11 +0200

    add workaround for spotify 'total' bug

--- spotify/src/main/java/rocks/metaldetector/spotify/client/transformer/SpotifyArtistSearchResultTransformer.java
+++ spotify/src/main/java/rocks/metaldetector/spotify/client/transformer/SpotifyArtistSearchResultTransformer.java
@@ -34,13 +34,17 @@ public class SpotifyArtistSearchResultTransformer {
   }
 
   private int calculateTotalPages(SpotifyArtistSearchResult searchResult) {
-    return searchResult.getTotal() % searchResult.getLimit() == 0 ? searchResult.getTotal() / searchResult.getLimit()
-                                                                  : searchResult.getTotal() / searchResult.getLimit() + 1;
+    int total = Math.min(searchResult.getTotal(), 2000); // Spotify Bug Workaround (see https://bit.ly/34iuJ3Q)
+    return divideAndRoundUp(total, searchResult.getLimit());
   }
 
   private List<SpotifyArtistDto> transformArtistSearchResults(List<SpotifyArtist> results) {
     return results.stream()
         .map(artistTransformer::transform)
         .collect(Collectors.toList());
+  }
+
+  private int divideAndRoundUp(int dividend, int divisor) {
+    return (int) Math.ceil((double) dividend / (double) divisor);
   }
 }
