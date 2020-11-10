6aae599da2d5d9a271f1722fa2d598801a37c09f
Daniel Wagner <wagner.daniel87@gmail.com>
2020-11-10 17:51:11 +0100

    remove SETTINGS endpoint and view

--- webapp/src/main/java/rocks/metaldetector/config/constants/ViewNames.java
+++ webapp/src/main/java/rocks/metaldetector/config/constants/ViewNames.java
@@ -22,8 +22,6 @@ public class ViewNames {
   public static class Frontend {
     public static final String HOME                    = "frontend/home";
     public static final String SEARCH                  = "frontend/search";
-    public static final String SETTINGS                = "frontend/settings";
-    public static final String PROFILE                 = "frontend/profile";
     public static final String RELEASES                = "frontend/releases";
     public static final String BLOG                    = "frontend/blog";
     public static final String MY_ARTISTS              = "frontend/my-artists";
