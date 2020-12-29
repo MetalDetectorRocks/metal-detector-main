e5afc166aa99809745cc2549949c842c4adfd56a
Daniel Wagner <wagner.daniel87@gmail.com>
2020-12-29 13:00:57 +0100

    remove requiresChannel from SecurityConfig because it is handled on proxy layer

--- webapp/src/main/java/rocks/metaldetector/security/SecurityConfig.java
+++ webapp/src/main/java/rocks/metaldetector/security/SecurityConfig.java
@@ -57,8 +57,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-      .requiresChannel().requestMatchers(request -> request.getHeader("X-Forwarded-Proto") != null).requiresSecure()
-      .and()
       .csrf().ignoringAntMatchers(REST_ENDPOINTS, ACTUATOR_ENDPOINTS)
       .and()
       .authorizeRequests()
