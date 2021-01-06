07cebc992747978c01cf272a56d177c66addea25
Nils Dammenhayn <nils.dammenhayn@europace.de>
2021-01-06 09:36:07 +0100

    Caching deactivated for SpringBoot-Tests

--- webapp/src/test/java/rocks/metaldetector/MetalDetectorApplicationDefaultIT.java
+++ webapp/src/test/java/rocks/metaldetector/MetalDetectorApplicationDefaultIT.java
@@ -9,7 +9,8 @@ import rocks.metaldetector.testutil.WithIntegrationTestConfig;
 @SpringBootTest
 @ActiveProfiles("default")
 @TestPropertySource(properties = {
-    "spring.datasource.url=jdbc:h2:mem:test-default;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE"
+    "spring.datasource.url=jdbc:h2:mem:test-default;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+    "spring.cache.type=none"
 })
 class MetalDetectorApplicationDefaultIT implements WithIntegrationTestConfig {
 
