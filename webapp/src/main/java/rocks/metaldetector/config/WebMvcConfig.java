f59aee88f20f3ba807c1366c7d6c490e33e6fa23
Daniel Wagner <wagner.daniel87@gmail.com>
2021-01-09 17:06:06 +0100

    add legal pages imprint and privacy policy

--- webapp/src/main/java/rocks/metaldetector/config/WebMvcConfig.java
+++ webapp/src/main/java/rocks/metaldetector/config/WebMvcConfig.java
@@ -24,10 +24,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
+    // Guest paged
+    registry.addViewController(Endpoints.Guest.IMPRINT).setViewName(ViewNames.Guest.IMPRINT);
+    registry.addViewController(Endpoints.Guest.PRIVACY_POLICY).setViewName(ViewNames.Guest.PRIVACY_POLICY);
+
     // Frontend pages
     registry.addViewController(Endpoints.Frontend.BLOG).setViewName(ViewNames.Frontend.BLOG);
     registry.addViewController(Endpoints.Frontend.RELEASES).setViewName(ViewNames.Frontend.RELEASES);
     registry.addViewController(Endpoints.Frontend.IMPRINT).setViewName(ViewNames.Frontend.IMPRINT);
+    registry.addViewController(Endpoints.Frontend.PRIVACY_POLICY).setViewName(ViewNames.Frontend.PRIVACY_POLICY);
     registry.addViewController(Endpoints.Frontend.ACCOUNT_DETAILS).setViewName(ViewNames.Frontend.ACCOUNT_DETAILS);
     registry.addViewController(Endpoints.Frontend.SPOTIFY_SYNCHRONIZATION).setViewName(ViewNames.Frontend.SPOTIFY_SYNCHRONIZATION);
     registry.addViewController(Endpoints.Frontend.NOTIFICATION_SETTINGS).setViewName(ViewNames.Frontend.NOTIFICATION_SETTINGS);
@@ -49,7 +54,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(redirectionHandlerInterceptor).addPathPatterns(Endpoints.AntPattern.AUTH_PAGES);
+    registry.addInterceptor(redirectionHandlerInterceptor).addPathPatterns(Endpoints.AntPattern.GUEST_PAGES);
   }
 
   @Bean
