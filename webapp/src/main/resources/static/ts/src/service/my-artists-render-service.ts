6371f2fd2df4b9a210912147bd7ba6937f6ae980
Daniel Wagner <wagner.daniel87@gmail.com>
2021-01-13 18:34:37 +0100

    show hint to synchronize artists from spotify if my artists page is empty

--- webapp/src/main/resources/static/ts/src/service/my-artists-render-service.ts
+++ webapp/src/main/resources/static/ts/src/service/my-artists-render-service.ts
@@ -32,7 +32,8 @@ export class MyArtistsRenderService extends AbstractRenderService<MyArtistsRespo
 
     protected onRendering(response: MyArtistsResponse): void {
         if (response.myArtists.length === 0 && response.pagination.currentPage === 1) {
-            const message = '<h3 class="h5">Currently you do not follow any artist.</h3>Start a search for your favorite artists right now.';
+            const message = '<h3 class="h5">Currently you do not follow any artist.</h3>Start a search for your favorite artists ' +
+                'or <a href="/settings/spotify-synchronization">synchronize</a> your artists from your Spotify account.';
             const infoMessage = this.alertService.renderInfoAlert(message, false);
             this.hostElement.insertAdjacentElement("afterbegin", infoMessage);
         }
