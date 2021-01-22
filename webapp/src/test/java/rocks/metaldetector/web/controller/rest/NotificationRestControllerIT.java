809dddc80c7d4f7627fa0cdd1fe7022990b3d28f
hedgehog1833 <32484683+hedgehog1833@users.noreply.github.com>
2021-01-22 17:00:49 +0100

    NotificationService can notify on release date depending on NotificationConfig https://trello.com/c/hlDkMGvu

--- webapp/src/test/java/rocks/metaldetector/web/controller/rest/NotificationRestControllerIT.java
+++ webapp/src/test/java/rocks/metaldetector/web/controller/rest/NotificationRestControllerIT.java
@@ -11,7 +11,8 @@ import rocks.metaldetector.testutil.BaseWebMvcTestWithSecurity;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static rocks.metaldetector.support.Endpoints.Rest.NOTIFY;
+import static rocks.metaldetector.support.Endpoints.Rest.NOTIFICATION_ON_FREQUENCY;
+import static rocks.metaldetector.support.Endpoints.Rest.NOTIFICATION_ON_RELEASE_DATE;
 
 @WebMvcTest(controllers = NotificationRestController.class)
 public class NotificationRestControllerIT extends BaseWebMvcTestWithSecurity {
@@ -24,11 +25,19 @@ public class NotificationRestControllerIT extends BaseWebMvcTestWithSecurity {
   class AdministratorRoleTest {
 
     @Test
-    @DisplayName("Administrator is allowed to POST on endpoint " + NOTIFY + "'")
+    @DisplayName("Administrator is allowed to POST on endpoint " + NOTIFICATION_ON_FREQUENCY + "'")
     @WithMockUser(roles = "ADMINISTRATOR")
-    void admin_is_allowed_to_notify_all_users() throws Exception {
-      mockMvc.perform(post(NOTIFY))
+    void admin_is_allowed_to_notify_on_frequency() throws Exception {
+      mockMvc.perform(post(NOTIFICATION_ON_FREQUENCY))
               .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Administrator is allowed to POST on endpoint " + NOTIFICATION_ON_RELEASE_DATE + "'")
+    @WithMockUser(roles = "ADMINISTRATOR")
+    void admin_is_allowed_to_notify_on_release_date() throws Exception {
+      mockMvc.perform(post(NOTIFICATION_ON_RELEASE_DATE))
+          .andExpect(status().isOk());
     }
   }
 
@@ -37,11 +46,19 @@ public class NotificationRestControllerIT extends BaseWebMvcTestWithSecurity {
   class UserRoleTest {
 
     @Test
-    @DisplayName("User is not allowed to POST on endpoint " + NOTIFY + "'")
+    @DisplayName("User is not allowed to POST on endpoint " + NOTIFICATION_ON_RELEASE_DATE + "'")
     @WithMockUser(roles = "USER")
-    void user_is_not_allowed_to_notify_all_users() throws Exception {
-      mockMvc.perform(post(NOTIFY))
+    void user_is_not_allowed_to_notify_on_frequency() throws Exception {
+      mockMvc.perform(post(NOTIFICATION_ON_FREQUENCY))
               .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @DisplayName("User is not allowed to POST on endpoint " + NOTIFICATION_ON_RELEASE_DATE + "'")
+    @WithMockUser(roles = "USER")
+    void user_is_not_allowed_to_notify_on_release_date() throws Exception {
+      mockMvc.perform(post(NOTIFICATION_ON_RELEASE_DATE))
+          .andExpect(status().isForbidden());
     }
   }
 }
