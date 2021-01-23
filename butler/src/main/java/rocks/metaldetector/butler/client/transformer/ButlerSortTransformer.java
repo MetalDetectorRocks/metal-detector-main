52889900cf3477a1684b7221b4d69b101b5cf060
Daniel Wagner <wagner.daniel87@gmail.com>
2021-01-23 18:04:13 +0100

    add ButlerSortTransformer that transform a DetectorSort in URL params for butler requests

---
+++ butler/src/main/java/rocks/metaldetector/butler/client/transformer/ButlerSortTransformer.java
@@ -0,0 +1,22 @@
+package rocks.metaldetector.butler.client.transformer;
+
+import org.springframework.stereotype.Component;
+import rocks.metaldetector.support.DetectorSort;
+
+import java.util.Map;
+
+@Component
+public class ButlerSortTransformer {
+
+  private static final Map<String, String> FIELD_MAPPING = Map.of(
+    "release_date", "releaseDate",
+    "announcement_date", "createdDateTime"
+  );
+
+  private static final String DEFAULT_SORTING = "&sort=artist,ASC&sort=albumTitle,ASC";
+
+  public String transformSortParams(DetectorSort sort) {
+    String sortParam = String.format("sort=%s,%s", FIELD_MAPPING.getOrDefault(sort.getField(), sort.getField()), sort.getDirection());
+    return sortParam.concat(DEFAULT_SORTING);
+  }
+}
