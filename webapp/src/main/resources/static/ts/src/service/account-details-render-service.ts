cd241917fb237741a7df88e772fc5813df1e0705
Daniel Wagner <wagner.daniel87@gmail.com>
2021-02-06 15:45:40 +0100

    render server error message for email update

--- webapp/src/main/resources/static/ts/src/service/account-details-render-service.ts
+++ webapp/src/main/resources/static/ts/src/service/account-details-render-service.ts
@@ -1,42 +1,64 @@
 import { AccountDetailsRestClient } from "../clients/account-details-rest-client";
-import { ToastService } from "./toast-service";
+import { AlertService } from "./alert-service";
+import { UNKNOWN_ERROR_MESSAGE } from "../config/messages.config";
 
 export class AccountDetailsRenderService {
-    private readonly toastService: ToastService;
+    private readonly alertService: AlertService;
     private readonly accountDetailsRestClient: AccountDetailsRestClient;
+    private readonly emailInput: HTMLInputElement;
+    private readonly updateEmailButton: HTMLButtonElement;
+    private readonly emailUpdateErrorMessageHost: HTMLDivElement;
+    private readonly deleteAccountButton: HTMLButtonElement;
 
-    constructor(toastService: ToastService, accountDetailsRestClient: AccountDetailsRestClient) {
-        this.toastService = toastService;
+    constructor(alertService: AlertService, accountDetailsRestClient: AccountDetailsRestClient) {
+        this.alertService = alertService;
         this.accountDetailsRestClient = accountDetailsRestClient;
+        this.emailInput = document.getElementById("email-address") as HTMLInputElement;
+        this.emailUpdateErrorMessageHost = document.getElementById(
+            "email-update-error-message-wrapper",
+        ) as HTMLDivElement;
+        this.updateEmailButton = document.getElementById("update-email-address") as HTMLButtonElement;
+        this.deleteAccountButton = document.getElementById("delete-account") as HTMLButtonElement;
     }
 
     public init(): void {
-        const response = this.accountDetailsRestClient.getAccountDetails();
-        response.then((response) => {
-            if (response.email) {
-                const inputElement = document.getElementById("email-address") as HTMLInputElement;
-                inputElement.value = response.email;
-            }
-        });
+        this.accountDetailsRestClient
+            .getAccountDetails()
+            .then((response) => (this.emailInput.value = response.email))
+            .catch(() => this.renderEmailUpdateServerError(UNKNOWN_ERROR_MESSAGE));
 
-        const updateEmailAddressButton = document.getElementById("update-email-address") as HTMLButtonElement;
-        updateEmailAddressButton.addEventListener("click", this.onUpdateEmailAddressClicked.bind(this));
-
-        const deleteAccountButton = document.getElementById("delete-account") as HTMLButtonElement;
-        deleteAccountButton.addEventListener("click", this.onDeleteAccountClicked.bind(this));
+        this.updateEmailButton.addEventListener("click", this.onUpdateEmailAddressClicked.bind(this));
+        this.deleteAccountButton.addEventListener("click", this.onDeleteAccountClicked.bind(this));
     }
 
     private onUpdateEmailAddressClicked(): void {
-        const inputElement = document.getElementById("email-address") as HTMLInputElement;
+        this.clearErrorMessageHost();
         this.accountDetailsRestClient
-            .updateEmailAddress(inputElement.value)
-            .then((response) => (inputElement.value = response))
-            .then(() => this.toastService.createInfoToast("Successfully updated email address!"));
+            .updateEmailAddress(this.emailInput.value)
+            .then((response) => {
+                this.emailInput.value = response;
+                this.emailInput.classList.add("is-valid");
+                this.emailInput.classList.remove("is-invalid");
+            })
+            .catch((error) => {
+                this.emailInput.classList.add("is-invalid");
+                this.emailInput.classList.remove("is-valid");
+                this.renderEmailUpdateServerError(error.response.data.messages);
+            });
     }
 
     private onDeleteAccountClicked(): void {
         this.accountDetailsRestClient.deleteAccount().then(() => {
             window.location.href = "/";
         });
+    }
+
+    private renderEmailUpdateServerError(message: string): void {
+        const alert = this.alertService.renderErrorAlert(message, true);
+        this.emailUpdateErrorMessageHost.insertAdjacentElement("afterbegin", alert);
+    }
+
+    private clearErrorMessageHost(): void {
+        this.emailUpdateErrorMessageHost.innerHTML = "";
     }
 }
