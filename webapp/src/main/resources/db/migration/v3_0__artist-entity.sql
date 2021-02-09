7cfd967e834248a8b60d7106224ed0278a0bd3be
Daniel Wagner <wagner.daniel87@gmail.com>
2021-02-09 18:38:11 +0100

    use old thumb image as default for all new image columns

--- webapp/src/main/resources/db/migration/v3_0__artist-entity.sql
+++ webapp/src/main/resources/db/migration/v3_0__artist-entity.sql
@@ -1,9 +1,8 @@
 -- Creation Date: 2021-02-07
--- Description: Renames column 'thumb' to image_l
---              adds new columns: external_url, genres, spotify_popularity, spotify_follower, image_xs, image_s, image_m
-
-alter table artists
-rename column thumb TO image_l;
+-- Description:
+-- - adds new columns: external_url, genres, spotify_popularity, spotify_follower, image_xs, image_s, image_m
+-- - copies value from column thumb to image_xs, image_s, image_m
+-- - renames column thumb to image_l
 
 alter table artists
 add column external_url varchar(255),
@@ -13,3 +12,12 @@ add column spotify_follower integer,
 add column image_xs varchar(255),
 add column image_s varchar(255),
 add column image_m varchar(255);
+
+update artists
+set image_xs = thumb,
+    image_s = thumb,
+    image_m = thumb
+where thumb is not null;
+
+alter table artists
+rename column thumb TO image_l;
