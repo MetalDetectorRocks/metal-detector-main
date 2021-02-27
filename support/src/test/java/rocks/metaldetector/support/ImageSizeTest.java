984aee68bef39bb7f54178414f38a4accfb20b8b
Daniel Wagner <wagner.daniel87@gmail.com>
2021-02-27 14:55:34 +0100

    move ImageSizeTest.java

--- support/src/test/java/rocks/metaldetector/support/ImageSizeTest.java
+++ support/src/test/java/rocks/metaldetector/support/ImageSizeTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import rocks.metaldetector.support.ImageSize;
 
 import java.util.stream.Stream;
 
