17e5b6121a7c4da59efc1d4b79a72f775a156caf
Nils Dammenhayn <nils.dammenhayn@europace.de>
2021-03-14 19:52:55 +0100

    Integration Test repaired, Bean added to TestConfig https://trello.com/c/xAzXMCzo

--- webapp/src/test/java/rocks/metaldetector/testutil/BaseWebMvcTestWithSecurity.java
+++ webapp/src/test/java/rocks/metaldetector/testutil/BaseWebMvcTestWithSecurity.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 import rocks.metaldetector.security.NonceSupplier;
 import rocks.metaldetector.security.RedirectionHandlerInterceptor;
 import rocks.metaldetector.service.user.UserService;
@@ -17,10 +21,14 @@ import rocks.metaldetector.support.SecurityProperties;
 import javax.sql.DataSource;
 
 @WebMvcTest
+@Import(BaseWebMvcTestWithSecurity.TestConfig.class)
 public abstract class BaseWebMvcTestWithSecurity implements WithSecurityConfig {
 
   @Autowired
   protected MockMvc mockMvc;
+
+  @Autowired
+  protected HandlerMappingIntrospector mvcHandlerMappingIntrospector;
 
   @SpyBean
   protected BCryptPasswordEncoder passwordEncoder;
@@ -49,4 +57,12 @@ public abstract class BaseWebMvcTestWithSecurity implements WithSecurityConfig {
   @MockBean
   protected HttpSecurity httpSecurity;
 
+  @Configuration
+  static class TestConfig {
+
+    @Bean
+    public HandlerMappingIntrospector mvcHandlerMappingIntrospector() {
+      return new HandlerMappingIntrospector();
+    }
+  }
 }
