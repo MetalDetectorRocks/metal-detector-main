48111f15c382d4f9201cec2b3dd43cb7a012ca1a
Nils Dammenhayn <nils.dammenhayn@europace.de>
2021-05-02 18:08:33 +0200

    Review: small findings

--- telegram/src/main/java/rocks/metaldetector/telegram/config/TelegramProperties.java
+++ telegram/src/main/java/rocks/metaldetector/telegram/config/TelegramProperties.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource(value = "classpath:application.yml")
@@ -13,14 +12,11 @@ import org.springframework.core.env.Environment;
 @Data
 public class TelegramProperties {
 
-  private final Environment environment;
   private final String restBaseUrl;
   private final String botId;
 
-  public TelegramProperties(Environment environment,
-                            @Value("${rest-base-url}") String restBaseUrl,
+  public TelegramProperties(@Value("${rest-base-url}") String restBaseUrl,
                             @Value("${bot-id}") String botId) {
-    this.environment = environment;
     this.restBaseUrl = restBaseUrl;
     this.botId = botId;
   }
