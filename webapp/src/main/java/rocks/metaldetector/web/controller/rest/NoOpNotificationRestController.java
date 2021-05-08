04ae75dd3fc62d31ca4dba17fbd4e5c810937a7c
Nils Dammenhayn <nils.dammenhayn@europace.de>
2021-05-08 13:59:08 +0200

    NoOpController for prod added

--- webapp/src/main/java/rocks/metaldetector/web/controller/rest/NoOpNotificationRestController.java
+++ webapp/src/main/java/rocks/metaldetector/web/controller/rest/NoOpNotificationRestController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import rocks.metaldetector.service.notification.NotificationService;
 
 import static rocks.metaldetector.support.Endpoints.Rest.NOTIFICATION_ON_ANNOUNCEMENT_DATE;
 import static rocks.metaldetector.support.Endpoints.Rest.NOTIFICATION_ON_FREQUENCY;
@@ -15,26 +14,21 @@ import static rocks.metaldetector.support.Endpoints.Rest.NOTIFICATION_ON_RELEASE
 @RestController
 @AllArgsConstructor
 @PreAuthorize("hasRole('ROLE_ADMINISTRATOR')")
-@Profile("!prod")
-public class NotificationRestController {
-
-  private final NotificationService notificationService;
+@Profile("prod")
+public class NoOpNotificationRestController {
 
   @PostMapping(path = NOTIFICATION_ON_FREQUENCY)
   public ResponseEntity<Void> notifyOnFrequency() {
-    notificationService.notifyOnFrequency();
     return ResponseEntity.ok().build();
   }
 
   @PostMapping(path = NOTIFICATION_ON_RELEASE_DATE)
   public ResponseEntity<Void> notifyOnReleaseDate() {
-    notificationService.notifyOnReleaseDate();
     return ResponseEntity.ok().build();
   }
 
   @PostMapping(path = NOTIFICATION_ON_ANNOUNCEMENT_DATE)
   public ResponseEntity<Void> notifyOnAnnouncementDate() {
-    notificationService.notifyOnAnnouncementDate();
     return ResponseEntity.ok().build();
   }
 }
