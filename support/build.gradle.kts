625214742d6f267bf7795977ebf4ea40333bcba6
Nils Dammenhayn <nils.dammenhayn@europace.de>
2021-09-02 21:46:11 +0200

    Dependency updates, gradle update, build modules in parallel and gradle cache activated

--- support/build.gradle.kts
+++ support/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
   implementation("org.apache.commons:commons-text")
   implementation("org.apache.httpcomponents:httpclient:${rootProject.extra["httpClientVersion"]}")
-  implementation("org.springframework:spring-jdbc:${rootProject.extra["springJdbcVersion"]}")
+  implementation("org.springframework:spring-jdbc:${rootProject.extra["springVersion"]}")
   api("org.springframework.security:spring-security-oauth2-client:${rootProject.extra["springSecurityVersion"]}")
   api("io.jsonwebtoken:jjwt:${rootProject.extra["jsonwebtokenVersion"]}")
   testImplementation("org.simplify4u:slf4j-mock:${rootProject.extra["slf4jMockVersion"]}")
