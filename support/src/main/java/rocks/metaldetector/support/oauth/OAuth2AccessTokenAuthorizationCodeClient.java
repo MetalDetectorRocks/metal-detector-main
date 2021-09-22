be70da4e1ca08a3d24d0131ccadb3b5e780fb4be
hedgehog1833 <nilsdam@aol.com>
2021-09-22 21:43:37 +0200

    Refresh token is fetched if access token is expired

--- support/src/main/java/rocks/metaldetector/support/oauth/OAuth2AccessTokenAuthorizationCodeClient.java
+++ support/src/main/java/rocks/metaldetector/support/oauth/OAuth2AccessTokenAuthorizationCodeClient.java
@@ -1,9 +1,15 @@
 package rocks.metaldetector.support.oauth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
 
 @Component
 @RequiredArgsConstructor
@@ -11,16 +17,33 @@ public class OAuth2AccessTokenAuthorizationCodeClient implements OAuth2AccessTok
 
   private final OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
   private final CurrentOAuthUserIdSupplier currentOAuthUserIdSupplier;
+  private final OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager;
   private String registrationId;
 
   @Override
   public String getAccessToken() {
     String userId = currentOAuthUserIdSupplier.get();
     OAuth2AuthorizedClient authorizedClient = oAuth2AuthorizedClientService.loadAuthorizedClient(registrationId, userId);
-    if (authorizedClient == null || authorizedClient.getAccessToken() == null ||
-        authorizedClient.getAccessToken().getTokenValue() == null || authorizedClient.getAccessToken().getTokenValue().isEmpty()) {
-      throw new IllegalArgumentException("No access token for client '" + registrationId + "'");
+
+    if (authorizedClient == null || authorizedClient.getAccessToken().getExpiresAt() == null) {
+      throw new IllegalArgumentException("No access token for client '" + userId + "' and registrationId '" + registrationId + "'");
     }
+
+    if (authorizedClient.getAccessToken().getExpiresAt().isBefore(Instant.now())) {
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      OAuth2AuthorizeRequest request = OAuth2AuthorizeRequest
+          .withAuthorizedClient(authorizedClient)
+          .principal(authentication)
+          .build();
+      OAuth2AuthorizedClient reAuthorizedClient = oAuth2AuthorizedClientManager.authorize(request);
+
+      if (reAuthorizedClient == null) {
+        throw new IllegalArgumentException("No refreshed access token for client '" + userId + "' and registrationId '" + registrationId + "'");
+      }
+
+      return reAuthorizedClient.getAccessToken().getTokenValue();
+    }
+
     return authorizedClient.getAccessToken().getTokenValue();
   }
 
