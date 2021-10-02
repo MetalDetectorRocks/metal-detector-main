ed478f6e43db86d180170c6f222f0ca605025d54
Daniel Wagner <wagner.daniel87@gmail.com>
2021-10-02 20:32:44 +0200

    add swiper from js lib

--- webapp/src/main/resources/static/ts/webpack.config.prod.js
+++ webapp/src/main/resources/static/ts/webpack.config.prod.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const webpack = require('webpack');
+const webpack = require("webpack");
 const CleanPlugin = require("clean-webpack-plugin");
 
 module.exports = {
@@ -11,7 +11,7 @@ module.exports = {
         homepage: "./src/bundles/homepage.ts",
         spotify_synchronization: "./src/bundles/spotify-synchronization.ts",
         account_details: "./src/bundles/account-details.ts",
-        notification_settings: "./src/bundles/notification-settings.ts"
+        notification_settings: "./src/bundles/notification-settings.ts",
     },
     output: {
         filename: "[name].bundle.js",
@@ -22,15 +22,19 @@ module.exports = {
             {
                 test: /\.ts$/,
                 use: "ts-loader",
-                exclude: /node_modules/
-            }
-        ]
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
     },
     resolve: {
-        extensions: [".ts", ".js"]
+        extensions: [".ts", ".js"],
     },
     plugins: [
         // cleans the output folder before writing to it
-        new CleanPlugin.CleanWebpackPlugin()
-    ]
+        new CleanPlugin.CleanWebpackPlugin(),
+    ],
 };
