bf020d1a138164af632a3f8de37c340c3823f8ef
hedgehog1833 <nilsdam@aol.com>
2021-10-21 23:15:43 +0200

    Most expected releases sorting and number of releases fixed

--- webapp/src/main/java/rocks/metaldetector/service/summary/ReleaseCollector.java
+++ webapp/src/main/java/rocks/metaldetector/service/summary/ReleaseCollector.java
@@ -11,6 +11,7 @@ import rocks.metaldetector.support.PageRequest;
 import rocks.metaldetector.support.TimeRange;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -26,20 +27,26 @@ import static rocks.metaldetector.support.DetectorSort.Direction.DESC;
 @AllArgsConstructor
 public class ReleaseCollector {
 
+  static final int PAGE_SIZE = 50;
+
   private final ReleaseService releaseService;
 
   public List<ReleaseDto> collectUpcomingReleases(List<ArtistDto> artists) {
     LocalDate tomorrow = LocalDate.now().plusDays(1);
     TimeRange timeRange = new TimeRange(tomorrow, tomorrow.plusMonths(TIME_RANGE_MONTHS));
     DetectorSort sort = new DetectorSort("releaseDate", ASC);
-    return collectReleases(artists, timeRange, sort);
+    return collectReleases(artists, timeRange, sort).stream()
+        .limit(RESULT_LIMIT)
+        .collect(Collectors.toList());
   }
 
   public List<ReleaseDto> collectRecentReleases(List<ArtistDto> artists) {
     LocalDate now = LocalDate.now();
     TimeRange timeRange = new TimeRange(now.minusMonths(TIME_RANGE_MONTHS), now);
     DetectorSort sort = new DetectorSort("releaseDate", DESC);
-    return collectReleases(artists, timeRange, sort);
+    return collectReleases(artists, timeRange, sort).stream()
+        .limit(RESULT_LIMIT)
+        .collect(Collectors.toList());
   }
 
   public List<ReleaseDto> collectTopReleases(TimeRange timeRange, List<ArtistDto> artists, int maxReleases) {
@@ -49,9 +56,9 @@ public class ReleaseCollector {
     return collectReleases(artists, timeRange, new DetectorSort("artist", ASC)).stream()
         .sorted(Comparator.comparingInt(
                 (ReleaseDto release) -> followersPerArtist.get(release.getArtist().toLowerCase()))
-                    .reversed()
-                    .thenComparing(ReleaseDto::getReleaseDate))
+                    .reversed())
         .limit(maxReleases)
+        .sorted(Comparator.comparing(ReleaseDto::getReleaseDate))
         .collect(Collectors.toList());
   }
 
@@ -61,8 +68,17 @@ public class ReleaseCollector {
     }
 
     List<String> artistNames = artists.stream().map(ArtistDto::getArtistName).collect(Collectors.toList());
-    PageRequest pageRequest = new PageRequest(1, RESULT_LIMIT, sort);
-    Page<ReleaseDto> releasePage = releaseService.findReleases(artistNames, timeRange, null, pageRequest);
-    return releasePage.getItems();
+
+    List<ReleaseDto> releases = new ArrayList<>();
+    int currentPage = 1;
+    Page<ReleaseDto> releasePage;
+
+    do {
+      PageRequest pageRequest = new PageRequest(currentPage, PAGE_SIZE, sort);
+      releasePage = releaseService.findReleases(artistNames, timeRange, null, pageRequest);
+      releases.addAll(releasePage.getItems());
+    } while (currentPage++ < releasePage.getPagination().getTotalPages());
+
+    return releases;
   }
 }
