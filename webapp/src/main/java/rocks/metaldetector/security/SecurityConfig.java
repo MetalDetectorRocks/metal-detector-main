5a015333292815320c485107486f2e34e2de8a06
Nils Dammenhayn <nilsdam@aol.com>
2021-12-31 14:17:54 +0100

    Permission and feature policy headers

--- webapp/src/main/java/rocks/metaldetector/security/SecurityConfig.java
+++ webapp/src/main/java/rocks/metaldetector/security/SecurityConfig.java
@@ -115,8 +115,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
       .and()
         .cors()
       .and()
-        // These headers are set in the proxy, so disabled here
         .headers()
+          // Permission policy is the new feature policy but not yet supported everywhere, so we set both
+          .permissionsPolicy().policy("interest-cohort=()").and()
+          .featurePolicy("interest-cohort=()").and()
+          // These headers are set in the proxy, so disabled here
           .frameOptions().disable()
           .xssProtection().disable()
           .contentTypeOptions().disable()
