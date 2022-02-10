e2b68105d656090b6623690d75ec76ea77f54626
Daniel Wagner <wagner.daniel87@gmail.com>
2022-02-10 17:40:50 +0100

    add static imports

--- webapp/src/main/java/rocks/metaldetector/web/controller/mvc/PreviewIndexController.java
+++ webapp/src/main/java/rocks/metaldetector/web/controller/mvc/PreviewIndexController.java
@@ -4,15 +4,16 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-import rocks.metaldetector.support.Endpoints;
 
 import static rocks.metaldetector.config.constants.ViewNames.Authentication.PREVIEW_INDEX;
+import static rocks.metaldetector.support.Endpoints.Frontend.HOME;
+import static rocks.metaldetector.support.Endpoints.Frontend.INDEX;
 
 @Controller
 @Profile("preview")
 public class PreviewIndexController {
 
-  @GetMapping({Endpoints.Frontend.INDEX, Endpoints.Frontend.HOME})
+  @GetMapping({INDEX, HOME})
   public ModelAndView showPreviewIndex() {
     return new ModelAndView(PREVIEW_INDEX);
   }
