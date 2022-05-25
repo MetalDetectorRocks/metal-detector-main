b314c778bafc0a4e9082c51f0549a7894a4eab28
Nils Dammenhayn <nilsdam@aol.com>
2022-05-25 19:59:27 +0200

    Refactored deprecated config WebSecurityConfigurerAdapter

--- support/src/main/java/rocks/metaldetector/support/infrastructure/WebConfig.java
+++ support/src/main/java/rocks/metaldetector/support/infrastructure/WebConfig.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.AllArgsConstructor;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -14,7 +13,6 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -26,8 +24,6 @@ import static org.springframework.http.MediaType.TEXT_PLAIN;
 @Configuration
 @AllArgsConstructor
 public class WebConfig {
-
-  private final CloseableHttpClient httpClient;
 
   @Bean
   MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
@@ -62,18 +58,9 @@ public class WebConfig {
   }
 
   @Bean
-  public HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
+  public HttpComponentsClientHttpRequestFactory clientHttpRequestFactory(CloseableHttpClient httpClient) {
     HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
     clientHttpRequestFactory.setHttpClient(httpClient);
     return clientHttpRequestFactory;
-  }
-
-  @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplateBuilder()
-        .requestFactory(this::clientHttpRequestFactory)
-        .errorHandler(new CustomClientErrorHandler())
-        .messageConverters(List.of(jackson2HttpMessageConverter(), stringHttpMessageConverter()))
-        .build();
   }
 }
