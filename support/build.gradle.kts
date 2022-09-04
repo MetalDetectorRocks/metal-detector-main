43e6f2f28a5d567a276234c06e6c45a031a5a10b
Nils Dammenhayn <nilsdam@aol.com>
2022-09-04 14:58:47 +0200

    Set spring versions via gradle plugin

--- support/build.gradle.kts
+++ support/build.gradle.kts
@@ -3,20 +3,20 @@ plugins {
 }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-security:${libs.versions.springBoot.get()}")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework:spring-jdbc")
+  api("org.springframework.security:spring-security-oauth2-client")
   implementation("org.apache.commons:commons-text:${libs.versions.commonsText.get()}")
   implementation("org.apache.httpcomponents:httpclient:${libs.versions.httpClient.get()}")
-  implementation("org.springframework:spring-jdbc:${libs.versions.spring.get()}")
   implementation("org.projectlombok:lombok:${libs.versions.lombok.get()}")
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${libs.versions.jackson.get()}")
 
   annotationProcessor("org.projectlombok:lombok:${libs.versions.lombok.get()}")
 
-  api("org.springframework.security:spring-security-oauth2-client:${libs.versions.springSecurity.get()}")
   api("io.jsonwebtoken:jjwt:${libs.versions.jsonwebtoken.get()}")
 
   testImplementation("com.github.valfirst:slf4j-test:${libs.versions.slf4jTest.get()}")
-  testImplementation("org.springframework.boot:spring-boot-starter-test:${libs.versions.springBoot.get()}")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.mockito:mockito-inline:${libs.versions.mockito.get()}")
 }
 
