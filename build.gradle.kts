befdd7cb3b45de5a42ffe23d769f37e3386b2d1d
Nils Dammenhayn <nilsdam@aol.com>
2023-08-12 18:04:47 +0200

    Bump spring boot but stay on hibernate 6.1.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,8 @@ import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
 val javaVersion: JavaVersion = JavaVersion.VERSION_17
 
 val dependencyVersions = listOf(
-    "com.google.guava:guava:32.1.2-jre"
+    "com.google.guava:guava:32.1.2-jre",
+    "com.nimbusds:nimbus-jose-jwt:9.31"
 )
 
 val dependencyGroupVersions = mapOf<String, String>()
