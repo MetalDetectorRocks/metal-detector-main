79cd2873c3fb4ff456a71471fafaa24e73dead72
Nils Dammenhayn <nilsdam@aol.com>
2024-08-08 22:50:13 +0200

    refactor deprecated use of oidc user service

--- webapp/src/main/java/rocks/metaldetector/service/user/OAuth2UserServiceConfig.java
+++ webapp/src/main/java/rocks/metaldetector/service/user/OAuth2UserServiceConfig.java
@@ -6,20 +6,20 @@ import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.List;
 
 @Configuration
 public class OAuth2UserServiceConfig {
 
-  static final String GOOGLE_SCOPE_PROFILE = "https://www.googleapis.com/auth/userinfo.profile";
-  static final String GOOGLE_SCOPE_EMAIL = "https://www.googleapis.com/auth/userinfo.email";
+    private static final String GOOGLE_SCOPE_PROFILE = "https://www.googleapis.com/auth/userinfo.profile";
+    private static final String GOOGLE_SCOPE_EMAIL = "https://www.googleapis.com/auth/userinfo.email";
+    private static final List<String> ALLOWED_SCOPES = Arrays.asList(OidcScopes.PROFILE, OidcScopes.EMAIL, GOOGLE_SCOPE_PROFILE, GOOGLE_SCOPE_EMAIL);
 
-  @Bean
-  public OidcUserService oidcUserService() {
-    OidcUserService oidcUserService = new OidcUserService();
-    oidcUserService.setAccessibleScopes(new HashSet<>(
-        Arrays.asList(OidcScopes.PROFILE, OidcScopes.EMAIL, GOOGLE_SCOPE_PROFILE, GOOGLE_SCOPE_EMAIL)
-    ));
-    return oidcUserService;
-  }
+    @Bean
+    public OidcUserService oidcUserService() {
+        OidcUserService oidcUserService = new OidcUserService();
+        oidcUserService.setRetrieveUserInfo(oidcUserRequest ->
+                oidcUserRequest.getAccessToken().getScopes().stream().anyMatch(ALLOWED_SCOPES::contains));
+        return oidcUserService;
+    }
 }
